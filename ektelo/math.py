06bd737456b8398fb6f5eccadadce6a19426f8ba
George Bissias <gbiss@cs.umass.edu>
2018-10-02 16:12:20 -0400

    implement Mironov ulp random

--- ektelo/math.py
+++ ektelo/math.py
@@ -1,9 +1,8 @@
 import crlibm
-import decimal
 import numpy as np
 import random
+from scipy.stats import geom
 
-MAX_PREC = 1e-54
 
 def clamp(x, B):
     if x > B:
@@ -13,30 +12,18 @@ def clamp(x, B):
     
     return x
 
-def ulp(x):
-    x2 = np.nextafter(x, x+1)
-    d = x2-x
-    log = np.log10(d)
-
-    if log < 0:
-        return np.power(10, np.floor(log))
-    else:
-        return np.power(10, np.ceil(log))
-    return 
 
 def select_ulp_random():
-    target = 2**-53
-    cumm = 0.0
+    exp = geom.rvs(0.5) - 1
+    sig = float('0.'+str(int(''.join(random.choices(('0','1'), k=52)), 2))) 
 
-    while cumm < target:
-        r = random.random()
-        cumm += ulp(r)
+    return sig * 10**(-exp)
 
-    return r
 
 def Lambda(lm):
     return np.power(2, np.ceil(np.log2(lm)))
     
+
 def Lambda_round(x, Lam):
     mult = x / Lam
 
@@ -49,17 +36,16 @@ def Lambda_round(x, Lam):
 def get_B(lm, eps):
     mult = 2**-49
 
-    return 1/float(lm) * (1 - lm*eps)
+    return 1 / float(lm) * (1 - lm*eps)
 
-def snap_laplace(f, lm, B, sensitivity=1):
+
+def snap_laplace(f, lm, sensitivity=1, B=None):
+    if B is None:
+        B = 2e20 * lm
+
     f = f / sensitivity
-    #B = 2*lm
-    print('B', B)
     Lam = Lambda(lm)
-    print('Lam', Lam)
     s = 2*(random.random() > 0.5) - 1
-    print('s', s)
     r = select_ulp_random()
-    print('r', r)
-    print(clamp(f, B), s * lm * crlibm.log2_rd(r))
+
     return clamp(Lambda_round(clamp(f, B) + s * lm * crlibm.log2_rd(r), Lam), B)
