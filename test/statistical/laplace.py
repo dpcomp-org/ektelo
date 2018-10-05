e9f23e3bb65087ad628b034098fdffea0f1f2e81
George Bissias <gbiss@cs.umass.edu>
2018-10-05 16:14:46 -0400

    add statistical equivalence test

---
+++ test/statistical/laplace.py
@@ -0,0 +1,24 @@
+from ektelo import private_laplace as pl
+from scipy import stats
+import unittest
+
+
+class TestLaplace(unittest.TestCase):
+
+    def setUp(self):
+        self.threshold = 0.1
+
+    def test_snapping(self):
+        lm = 10
+
+        xs = []
+        ys = []
+        import math
+        import random
+        for i in range(1000):
+            r = 1e-52 + i
+            xs.append(pl.snap_laplace(1, lm, r=r))
+            ys.append(pl.snap_laplace(1.5, lm, r=r))
+
+        D, pvalue = stats.ks_2samp(xs, ys)
+        self.assertGreater(pvalue, self.threshold)
