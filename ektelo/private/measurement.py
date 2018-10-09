a26813a1f1fae02478f89ccbd2966083fd3e081f
George Bissias <gbiss@cs.umass.edu>
2018-10-09 10:37:52 -0400

    allow laplace operator to use snapping mechanism

--- ektelo/private/measurement.py
+++ ektelo/private/measurement.py
@@ -3,20 +3,29 @@ import math
 import numpy as np
 from ektelo import util
 from ektelo.operators import MeasurementOperator
+from ektelo.private_laplace import snap_laplace
+
+snap_v = np.vectorize(snap_laplace)
 
 
 class Laplace(MeasurementOperator):
 
-    def __init__(self, A, eps):
+    def __init__(self, A, eps, private_laplace=True):
         self.A = A
         self.eps = eps
+        self.private_laplace = private_laplace
 
     def measure(self, X, prng):
         sensitivity = self.sensitivity_L1(self.A)
         laplace_scale = util.old_div(sensitivity, float(self.eps))
-        noise = prng.laplace(0.0, laplace_scale, self.A.shape[0])
 
-        return self.A.dot(X) + noise
+        if self.private_laplace:
+            x_hat = snap_v(self.A.dot(X), laplace_scale)
+        else:
+            noise = prng.laplace(0.0, laplace_scale, self.A.shape[0])
+            x_hat = self.A.dot(X) + noise
+
+        return x_hat
 
     @staticmethod
     def sensitivity_L1(A):
