dc0e7cc01ab36ebbba16ae2067689d3c8fdf3aea
George Bissias <gbiss@cs.umass.edu>
2018-10-26 10:28:28 -0400

    vector: various tweaks

--- ektelo/vector.py
+++ ektelo/vector.py
@@ -19,44 +19,18 @@ class EkteloVector:
 
     doesn't work:
     """
-
     def __init__(self, vector):
         """ Instantiate a EkteloVector from an explicitly represented backing vector
         
         :param vector: a 1d numpy ndarray
         """
-        assert len(vector.shape) == 2 and (vector.shape[0] == 1 or vector.shape[1] == 1)
-
         self.vector = vector
         self.dtype = vector.dtype
         self.shape = vector.shape
-  
-    def asDict(self):
-        d = util.class_to_dict(self, ignore_list=[])
-        return d
+     
+    def __abs__(self):
+        return EkteloMatrix(self.vector.__abs__())
 
-    def transpose(self, *axes):
-        vector = self.vector.T
-
-        return EkteloVector(vector)
-    
-    def __mul__(self, other):
-        assert self.shape[1] == other.shape[0], 'incompatible dimensions for multiplication'
-
-        if np.isscalar(other):
-            return self.vector * other
-        elif isinstance(other, EkteloVector):
-            return self.vector * other.vector
-        elif isinstance(other, EkteloMatrix):
-            return EkteloVector((other * self.transpose().vector).transpose())
-        elif type(other) == np.ndarray:
-            return self.vector * other 
-        else:
-            raise TypeError('incompatible type %s for multiplication with EkteloVector')
-
-    def __sub__(self, other):
-        return -(-self + other)
-    
     def __add__(self, other):
         if np.isscalar(other):
             return self.vector + other
@@ -67,32 +41,57 @@ class EkteloVector:
             return EkteloVector(self.vector + other.vector)
         elif isinstance(other, EkteloMatrix):
             raise NotImplementedError('EkteloMatrix does not currently support addition')
+        elif type(other) == np.ndarray:
+            return EkteloVector(self.vector + other)
+        else:
+            raise TypeError('incompatible type %s for multiplication with EkteloVector')
+
+    def _adjoint(self):
+        return self._transpose()
+
+    def __getitem__(self, key):
+        return EkteloVector(self.vector.__getitem__(key))
+
+    def __len__(self):
+        return self.size()
+
+    def __mul__(self, other):
+        assert self.shape[-1] == other.shape[0], 'incompatible dimensions for multiplication'
+
+        if np.isscalar(other):
+            return self.vector * other
+        elif isinstance(other, EkteloVector):
+            return self.vector * other.vector
+        elif isinstance(other, EkteloMatrix):
+            return EkteloVector((other * self.transpose().vector).transpose())
+        elif type(other) == np.ndarray:
+            return EkteloMatrix(self.vector * other)
         else:
             raise TypeError('incompatible type %s for multiplication with EkteloVector')
 
     def __neg__(self):
         return EkteloVector(-self.vector)
 
+    def __sub__(self, other):
+        return -(-self + other)
+    
+    def asDict(self):
+        d = util.class_to_dict(self, ignore_list=[])
+        return d
+
+    def flatten(self, order='C'):
+        return EkteloVector(self.vector.flatten(order))
+
+    def size(self):
+        return int(np.prod(self.shape))
+
     def sum(self, axis=None):
         return EkteloVector(self.vector.sum())
 
-    def _adjoint(self):
-        return self._transpose()
-
-    def __getitem__(self, key):
-        """ 
-        return an entry from the vector
-    
-        :param key: the index of the entry to return
-        :return: entry
-        """
-        if self.shape[0] == 1:
-            return EkteloVector(self.vector[1,key])
-
-        return EkteloVector(self.vector[key,1])
-    
     def to_array(self):
         return self.vector
-    
-    def __abs__(self):
-        return EkteloMatrix(self.vector.__abs__())
+
+    def transpose(self, *axes):
+        vector = self.vector.T
+
+        return EkteloVector(vector)
