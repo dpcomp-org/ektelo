3e8ccc7bde548fa9e5e1be76434bfd52970f79f1
Ryan McKenna <rmckenna21@gmail.com>
2018-11-01 16:16:22 -0400

    update factored inference stuff

--- ektelo/client/inference_projected.py
+++ ektelo/client/inference_projected.py
@@ -2,6 +2,55 @@ import numpy as np
 from ektelo import matrix
 from scipy import sparse
 from ektelo.client.inference import multWeightsFast
+from collections import OrderedDict
+
+class Domain:
+    def __init__(self, attrs, shape):
+        """ Construct a Domain object
+        
+        :param attrs: a list or tuple of attribute names
+        :param shape: a list or tuple of domain sizes for each attribute
+        """
+        self.attrs = attrs
+        self.shape = shape
+        self.config = OrderedDict(zip(attrs, shape))
+        
+    def project(self, attrs):
+        """ project the domain onto a subset of attributes
+        
+        :param attrs: the attributes to project onto
+        :return: the projected Domain object
+        """
+        # return the projected domain
+        shape = tuple(self.config[a] for a in attrs)
+        return Domain(attrs, shape)
+    
+    def size(self, col):
+        """ return the size of an individual attribute
+
+        :param col: the attribute 
+        """
+        return self.config[col]
+    
+def projection_matrix(domain, proj):
+    """ Construct the projection matrix P that projects a data vector from the
+        full domain to a new datavector over a subset of attributes
+        
+    :param domain: A Domain object corresponding to the full domain
+    :param proj: a tuple of attributes, corresponding to the projected domain
+    :return: An ektelo matrix
+    """ 
+    new_domain = domain.project(proj)
+    subs = []
+    for a in domain.attrs:
+        n = domain.size(a)
+        if a in new_domain.attrs:
+            subs.append(matrix.Identity(n))
+        else:
+            subs.append(matrix.Ones(1,n))
+    
+    return matrix.Kronecker(subs)
+        
 
 class ProductDist:
     """ factored representation of data from MWEM paper """
@@ -48,14 +97,14 @@ def projected_vstack(measurement_cache, domain):
         y is a numpy array of noisy answers
         noise_scale is the sqrt of the variance of the noisy answers
         projection is the tuple of axes the measurements are taken over
-    :param domain: the domain size tuple
+    :param domain: the Domain object
     """
     Ms = []
     ys = []
 
-    for M, y, noise_scale, projection in measurement_cache:
+    for M, y, noise_scale, proj in measurement_cache:
         c = 1.0 / noise_scale
-        P = matrix.Project(domain, projection)
+        P = projection_matrix(domain, proj)
         Q = c * matrix._LazyProduct(M, P)
         Ms.append(Q)
         ys.append(c*y)
@@ -63,8 +112,8 @@ def projected_vstack(measurement_cache, domain):
 
 def _cluster(measurement_cache):
     """
-    Cluster the measurements into disjoint subsets by finding the connected components
-    of the graph implied by the measurement projections
+    Cluster the measurements into disjoint subsets by finding the connected 
+    components of the graph implied by the measurement projections
     """
     # create the adjacency matrix
     k = len(measurement_cache)
@@ -90,20 +139,28 @@ class FactoredMultiplicativeWeights:
 
     def infer(self, measurement_cache, total):
         groups, proj = _cluster(measurement_cache) # cluster measurements
+        
+        ans = { }
 
         for group, proj in zip(groups, proj):
-            subdom = tuple(self.domain[i] for i in proj)
-            n = np.prod(subdom)
+            subdom = domain.project(proj)
+            n = np.prod(subdom.shape)
             M, y = projected_vstack(group, subdom)
             hatx = np.ones(n) * total / n
             hatx = multWeightsFast(hatx, M, y, updateRounds=100) / total
+            ans[proj] = hatx
+        return ans
 
 if __name__ == '__main__':
+    
+    attrs = ('a', 'b')
+    shape = (10, 10)
+    domain = Domain(attrs, shape)
 
     I = matrix.Identity(10)
     y = np.ones(10)*10
 
-    measurement_cache = [(I, y, 1.0, (0,)), (I, y, 1.0, (1,))]
+    measurement_cache = [(I, y, 1.0, ('a',)), (I, y, 1.0, ('b',))]
     
-    mw = FactoredMultiplicativeWeights( (10, 10) )
+    mw = FactoredMultiplicativeWeights( domain )
     mw.infer(measurement_cache, 100) 
